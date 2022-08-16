345ed30cbeb676cd42010648daea6eba3204ae75
Tran Huy Chien <chienth@timebird.org>
2022-08-16 15:29:48 +0700

    test cuoi

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -67,6 +67,7 @@ public class ProductController {
     @GetMapping("/export")
     public ResponseEntity<Resource> exportListProduct() {
         System.out.println("test lỗi");
+        System.out.println("test cuoi");
         try {
             List<String> listHeader = Arrays.asList("No", "productName", "price", "description");
             DateFormat dateFormatter = new SimpleDateFormat("ddMMyy");
